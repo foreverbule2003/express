526b7bd6f3bb85b34c913cbea5dcf9c9253ee13a
tim <tim0907@settour.com.tw>
2019-09-27 18:02:14 +0800

    save data from express to sqlite

--- app.js
+++ app.js
@@ -2,45 +2,42 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const fs = require('fs')
-const QUEUE = 'queue.json';
-const STORE = 'store.json';
+// const fs = require('fs')
+const {
+  fileIoInit,
+  DBInit,
+  DBtableInit,
+  DBinsert,
+  DBquery,
+} = require('./model');
+const { get, isEmpty } = require('lodash');
+const myFun = () => {
+  console.log('qwer');
+}
+
 app.use(express.json())
 app.get('/', (req, res) => {
-    // console.log(req.body)
-    // console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-    const urls = JSON.stringify(req.body)
-    console.log(urls);
-    // fs.appendFile(QUEUE, JSON.stringify(req.body), (err) => {
-    //     if (err) throw err;
-    //     readfile()
-    //     console.log('save url params')
-    // })
+  fileIoInit();
+  const postedData = JSON.stringify(req.body);
+  const postedObj = JSON.parse(postedData);
+  const urls = get(postedObj, 'urls', []);
+  // console.log('url', urls[0]);
+  // console.log(DBInit());
+  if (isEmpty(urls)) throw Error('posted data error');
+  const db = DBInit();
+  db.serialize(() => {
+    // 如果 table 不在就建立 table
+    db.run(DBtableInit);
 
-    //     ;
+    // create data
+    urls.map(url => {
+      db.run(DBinsert, [url, "false"]);
+    })
+  });
 
-    res.send('hola')
+  db.close();
+  // fileIoInit(myFun);
+  res.send('express save data success');
 })
 
-readfile = () => {
-    console.log('start read queue');
-    const queue = fs.readFileSync(QUEUE);
-    const urls = JSON.parse(queue)
-    console.log(urls);
-    // console.log(JSON.parse(queue)[urls]);
-
-    // fs.readFile(QUEUE, 'utf8', (err, quere) => {
-    //     console.log(JSON.parse(quere)[urls]);
-
-    //     fs.readFile(STORE, 'utf8', (err, store) => {
-    //         console.log(store);
-    //         // console.log(JSON.parse(store));
-    //         console.log('Merge!!!!!');
-    //         // const newStore = [...store[urls], ...quere[urls]]
-    //         // console.log(newStore);
-    //     })
-
-    //     // if (!err) fs.unlinkSync(QUEUE);
-    // })
-}
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`listening on port ${port}!`))
--- model.js
+++ model.js
@@ -1,48 +1,82 @@
 const fs = require('fs');
 const FILE_PATH = './sqlite.db';
 
-fs.open(FILE_PATH, 'r', (err) => {
-  // The file exists!
-  if (err) {
-    fs.writeFile(FILE_PATH, '', (err) => {
-      if (err) console.log(err);
-      console.log("The file was created!");
-    });
-  }
+const fileIoInit = (startDB) => {
+  const FILE_PATH = './sqlite.db';
+  fs.open(FILE_PATH, 'r', (err) => {
+    // The file exists!
+    if (err) {
+      fs.writeFile(FILE_PATH, '', (err) => {
+        if (err) console.log(err);
+        console.log("The file was createdAt!");
+      });
+    }
+    console.log('load db file success');
+    // startDB();
+  });
+}
 
-  startDB();
-});
+// fs.open(FILE_PATH, 'r', (err) => {
+//   // The file exists!
+//   if (err) {
+//     fs.writeFile(FILE_PATH, '', (err) => {
+//       if (err) console.log(err);
+//       console.log("The file was createdAt!");
+//     });
+//   }
+//   startDB();
+// });
+
+const DBInit = () => {
+  const FILE_PATH = './sqlite.db';
+  const sqlite = require('sqlite3').verbose();
+  return new sqlite.Database(FILE_PATH);
+}
+
+const DBtableInit = "CREATE TABLE IF NOT EXISTS url (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT, done BOOLEAN, createdAt TEXT DEFAULT CURRENT_TIMESTAMP)";
+const DBinsert = "INSERT INTO url(content, done) VALUES(?,?)";
+const DBquery = "SELECT id, content, done, datetime(createdAt, 'localtime') as createdAt FROM url";
+const DBupdate = "update url set content=? where content=?";
+const DBupdated = "SELECT id, content FROM url where content=?"
 
 startDB = () => {
-  const file = FILE_PATH;
-  const sqlite = require('sqlite3').verbose();
-  const db = new sqlite.Database(file);
+  // const FILE_PATH = './sqlite.db';
+  // const sqlite = require('sqlite3').verbose();
+  // const db = new sqlite.Database(FILE_PATH);
+
+
+  const db = DBInit();
 
   db.serialize(() => {
     // 如果 table 不在就建立 table
-    db.run("CREATE TABLE IF NOT EXISTS url (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT, done BOOLEAN, created TEXT DEFAULT CURRENT_TIMESTAMP)");
+    db.run(DBtableInit);
 
     // create data
-    const insert = "INSERT INTO url(content, done) VALUES(?,?)";
-    db.run(insert, ["myUrl.com", "false"]);
+    db.run(DBinsert, ["myUrl.com", "false"]);
 
     // query data
-    const query = "SELECT id, content, done, datetime(created, 'localtime') as created FROM url";
-    db.each(query, (err, row) => {
-      console.log(`${row.id}: ${row.content}, ${row.done}, ${row.created}`);
+    db.each(DBquery, (err, row) => {
+      console.log(`${row.id}: ${row.content}, ${row.done}, ${row.createdAt}`);
     })
 
     // update date
-    const update = "update url set content=? where content=?";
-    db.run(update, ['asdf', 'myUrl.com']);
+    db.run(DBupdate, ['asdf', 'myUrl.com']);
 
     // query updated data
-    const updated = "SELECT id, content FROM url where content=?"
-    db.each(updated, 'asdf', (err, row) => {
+    db.each(DBupdated, 'asdf', (err, row) => {
       // console.log(err);
       if (err) throw Error(err);
       console.log(`${row.id}: ${row.content}`);
     })
   })
   db.close();
+}
+
+module.exports = {
+  fileIoInit,
+  DBInit,
+  DBtableInit,
+  DBinsert,
+  DBquery,
+
 }
